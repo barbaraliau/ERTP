4e858a8ab2c225d1e28fab580f762ec5a7ead1f8
Chris Hibbert <hibbert@agoric.com>
2019-07-20 13:47:42 -0700

    checkpoint

--- core/contractHost.js
+++ core/contractHost.js
@@ -15,6 +15,34 @@ import { makeUniAssayConfigMaker } from './config/uniAssayConfig';
 import { makeMint } from './issuers';
 import makePromise from '../util/makePromise';
 
+function makeCollect(E, log) {
+  function collect(seatP, winPurseP, refundPurseP, name = 'collecting') {
+    const results = harden([
+      E(seatP)
+        .getWinnings()
+        .then(winnings => E(winPurseP).depositAll(winnings)),
+      // TODO Bug if we replace the comma above with the uncommented
+      // out ".then(_ => undefined)," below, somehow we end up trying
+      // to marshal an array with holes, rather than an array with
+      // undefined elements. This remains true whether we use
+      // Promise.all or allSettled
+      /* .then(_ => undefined), */
+      E(seatP)
+        .getRefund()
+        .then(refund => refund && E(refundPurseP).depositAll(refund)),
+    ]);
+    const doneP = allSettled(results);
+    E.resolve(doneP).then(([wins, refs]) => {
+      log(`${name} wins: `, wins, ` refs: `, refs);
+    });
+    // Use Promise.all here rather than allSettled in order to
+    // propagate rejection.
+    return Promise.all(results);
+  }
+  return harden(collect);
+}
+harden(makeCollect);
+
 /** Make a reusable host that can reliably install and execute contracts. */
 function makeContractHost(E, evaluate) {
   // Maps from seat identity to seats
@@ -169,33 +197,5 @@ No invites left`;
   return contractHost;
 }
 harden(makeContractHost);
-
-function makeCollect(E, log) {
-  function collect(seatP, winPurseP, refundPurseP, name = 'collecting') {
-    const results = harden([
-      E(seatP)
-        .getWinnings()
-        .then(winnings => E(winPurseP).depositAll(winnings)),
-      // TODO Bug if we replace the comma above with the uncommented
-      // out ".then(_ => undefined)," below, somehow we end up trying
-      // to marshal an array with holes, rather than an array with
-      // undefined elements. This remains true whether we use
-      // Promise.all or allSettled
-      /* .then(_ => undefined), */
-      E(seatP)
-        .getRefund()
-        .then(refund => refund && E(refundPurseP).depositAll(refund)),
-    ]);
-    const doneP = allSettled(results);
-    E.resolve(doneP).then(([wins, refs]) => {
-      log(`${name} wins: `, wins, ` refs: `, refs);
-    });
-    // Use Promise.all here rather than allSettled in order to
-    // propagate rejection.
-    return Promise.all(results);
-  }
-  return harden(collect);
-}
-harden(makeCollect);
 
 export { makeContractHost, makeCollect };
